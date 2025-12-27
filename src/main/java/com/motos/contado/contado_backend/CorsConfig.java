89ea1ed1a9a9e326ef0784af9eee81da56bee784
daniel <daniel.lizana01@gmail.com>
2025-12-27 02:32:45 -0500

    quitando contraseña

--- src/main/java/com/motos/contado/contado_backend/CorsConfig.java
+++ src/main/java/com/motos/contado/contado_backend/CorsConfig.java
@@ -17,14 +17,19 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",   // tu front dev
-                "https://mv-motors.com" // tu front en prod
+                "https://mv-motors.com",
+                "https://contado-backend-26647667439.europe-west1.run.app"// tu front en prod
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/login", config);
+        source.registerCorsConfiguration("/pas", config);
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
